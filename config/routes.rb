7adf2d39760d3781466ee3519c9b19d6ded97a9b
Devin Carlson <djoncarlson@gmail.com>
2016-10-30 22:14:48 -0700

    comment out devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :ideas
   root to: 'visitors#index'
-  devise_for :users
-  resources :users
-  resources :charges
+  #devise_for :users
+  #resources :users
+  #resources :charges
 end
